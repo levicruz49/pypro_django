34536d6f326d8def0913d3c9dffbcfdb2ab6f1f1
Levi Cruz <levicruz49@gmail.com>
2020-07-20 16:51:36 -0300

    Arrumando msg de erro
    close #12

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def home(request):
 
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return HttpResponse('<html><body>Ola amigo Django</body></html>', content_type='text/html')
