a68c5f39b9dd48b0afd377a85d1af083b924d0e9
Levi Cruz <levicruz49@gmail.com>
2020-07-29 15:19:16 -0300

    Criada página de indice de aulas
    close #42

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 440789232},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 251497668},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    videos = {
-        'motivacao': {
-            'titulo': 'Video Aperitivo: Motivação',
-            'vimeo_id': 440789232
-        },
-        'instalacao-windows': {
-            'titulo': 'Instalacao Windows',
-            'vimeo_id': 251497668
-        },
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
