fdc376be3fd1ec98b9f37a6b3441236e11453176
Levi Cruz <levicruz49@gmail.com>
2020-08-02 13:12:53 -0300

    Implementado Breacrumb eficiente
    close #75

--- pypro/settings.py
+++ pypro/settings.py
@@ -8,7 +8,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -33,6 +32,7 @@ INSTALLED_APPS = [
     'collectfast',
     'django.contrib.staticfiles',
     'ordered_model',
+    'django_extensions',
     'pypro.base',
     'pypro.aperitivos',
     'pypro.modulos',
@@ -86,7 +86,6 @@ DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=parse_database),
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -105,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -118,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
