4a0776b0bfcde2cf12eec73537ca276e0eba2fd8
Levi Cruz <levicruz49@gmail.com>
2020-08-02 22:12:02 -0300

    Criado botão login, botao sair, redirect para login e logout juntamente com o login_required
    close #83

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,7 +1,15 @@
 # Create your views here.
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from pypro.modulos import facade
+
+
+def indice(request):
+    ctx = {
+        'modulos': facade.listar_modulos_com_aulas()
+    }
+    return render(request, 'modulos/indice.html', ctx)
 
 
 def detalhe(request, slug):
@@ -10,13 +18,7 @@ def detalhe(request, slug):
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
 
 
+@login_required()
 def aula(request, slug):
     n = facade.econtrar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', {'aula': n})
-
-
-def indice(request):
-    ctx = {
-        'modulos': facade.listar_modulos_com_aulas()
-    }
-    return render(request, 'modulos/indice.html', ctx)
